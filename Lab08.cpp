9a08d473b035c7c3aff1d3f872403be924687e1c
abbyrhee <128726158+abbyrhee@users.noreply.github.com>
2024-11-13 00:28:49 -0500

    Update Lab08.cpp

--- Lab08.cpp
+++ Lab08.cpp
@@ -1,11 +1,189 @@
 // Lab08.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+
+
 #include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFPhysics.h>
+#include <vector>
+
+using namespace std;
+using namespace sf;
+using namespace sfp;
+
+const float KB_SPEED = 0.2;
+
+void LoadTex(Texture& tex, string filename) {
+    if (!tex.loadFromFile(filename)) {
+        cout << "Could not load " << filename << endl;
+    }
+}
+
+void MoveCrossbow(PhysicsSprite& crossbow, int elapsedMS) {
+    if (Keyboard::isKeyPressed(Keyboard::Right)) {
+        Vector2f newPos(crossbow.getCenter());
+        newPos.x = newPos.x + (KB_SPEED * elapsedMS);
+        crossbow.setCenter(newPos);
+    }
+    if (Keyboard::isKeyPressed(Keyboard::Left)) {
+        Vector2f newPos(crossbow.getCenter());
+        newPos.x = newPos.x - (KB_SPEED * elapsedMS);
+        crossbow.setCenter(newPos);
+    }
+}
+
+Vector2f GetTextSize(Text text) {
+    FloatRect r = text.getGlobalBounds();
+    return Vector2f(r.width, r.height);
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    RenderWindow window(VideoMode(800, 600), "Duck Hunter");
+    World world(Vector2f(0, 0));
+    int score(0);
+    int arrows(5);
+
+    PhysicsSprite& crossBow = *new PhysicsSprite();
+    Texture cbowTex;
+    LoadTex(cbowTex, "assets/crossbow.png");
+    crossBow.setTexture(cbowTex);
+    Vector2f sz = crossBow.getSize();
+    crossBow.setCenter(Vector2f(400,
+        600 - (sz.y / 2)));
+
+    PhysicsSprite arrow;
+    Texture arrowTex;
+    LoadTex(arrowTex, "assets/arrow.png");
+    arrow.setTexture(arrowTex);
+    bool drawingArrow(false);
+
+    PhysicsRectangle top;
+    top.setSize(Vector2f(800, 10));
+    top.setCenter(Vector2f(400, 5));
+    top.setStatic(true);
+    world.AddPhysicsBody(top);
+
+    PhysicsRectangle left;
+    left.setSize(Vector2f(10, 600));
+    left.setCenter(Vector2f(5, 300));
+    left.setStatic(true);
+    world.AddPhysicsBody(left);
+
+    PhysicsRectangle right;
+    right.setSize(Vector2f(10, 600));
+    right.setCenter(Vector2f(795, 300));
+    right.setStatic(true);
+    world.AddPhysicsBody(right);
+
+    Texture duckTex;
+    LoadTex(duckTex, "assets/duck.png");
+    PhysicsShapeList<PhysicsSprite> ducks;
+
+    top.onCollision = [&drawingArrow, &world, &arrow]
+    (PhysicsBodyCollisionResult result) {
+        drawingArrow = false;
+        world.RemovePhysicsBody(arrow);
+    };
+
+    Font fnt;
+    if (!fnt.loadFromFile("assets/arial.ttf")) {
+        cout << "Could not load font." << endl;
+        exit(3);
+    }
+    Clock clock;
+    Time lastTime(clock.getElapsedTime());
+    Time currentTime(lastTime);
+
+    long duckMS(0);
+    while ((arrows > 0) || drawingArrow) {
+        currentTime = clock.getElapsedTime();
+        Time deltaTime = currentTime - lastTime;
+        long deltaMS = deltaTime.asMilliseconds();
+        if (duckMS > 250) {
+            duckMS = 0;
+                PhysicsSprite& duck = ducks.Create();
+                duck.setTexture(duckTex);
+                Vector2f sz = duck.getSize();
+                duck.setCenter(Vector2f(100, 20 + (sz.y / 2)));
+                duck.setVelocity(Vector2f(0.8, 0));
+                world.AddPhysicsBody(duck);
+                duck.onCollision =
+                    [&drawingArrow, &world, &arrow, &duck, &ducks, &score, &right]
+                (PhysicsBodyCollisionResult result) {
+                    if (result.object2 == arrow) {
+                        drawingArrow = false;
+                        world.RemovePhysicsBody(arrow);
+                        world.RemovePhysicsBody(duck);
+                        ducks.QueueRemove(duck);
+                        score += 10;
+                    }
+                    if (result.object2 == right) {
+                        world.RemovePhysicsBody(duck);
+                        ducks.QueueRemove(duck);
+                    }
+                };
+        };
+
+        if (deltaMS > 9) {
+            duckMS = duckMS + deltaMS;
+            lastTime = currentTime;
+            world.UpdatePhysics(deltaMS);
+            MoveCrossbow(crossBow, deltaMS);
+
+            if (Keyboard::isKeyPressed(Keyboard::Space) &&
+                !drawingArrow) {
+                drawingArrow = true;
+                arrows = arrows - 1;
+                arrow.setCenter(crossBow.getCenter());
+                arrow.setVelocity(Vector2f(0, -1));
+                world.AddPhysicsBody(arrow);
+
+            }
+
+            window.clear();
+            if (drawingArrow) {
+                window.draw(arrow);
+            }
+
+            ducks.DoRemovals();
+            for (PhysicsShape& duck : ducks) {
+                window.draw((PhysicsSprite&)duck);
+            }
+
+            window.draw(crossBow);
+
+            Text arrowCountText;
+            arrowCountText.setFont(fnt);
+            arrowCountText.setString(to_string(arrows));
+            FloatRect tbounds = arrowCountText.getLocalBounds();
+            arrowCountText.setPosition(Vector2f(0, 580 - tbounds.height));
+            window.draw(arrowCountText);
+
+            Text scoreText;
+            scoreText.setFont(fnt);
+            scoreText.setString(to_string(score));
+            tbounds = scoreText.getLocalBounds();
+            scoreText.setPosition(Vector2f(780 - tbounds.width, 580 - tbounds.height));
+            window.draw(scoreText);
+
+            //world.VisualizeAllBounds(window);
+
+            window.display();
+        }
+
+    };
+    window.display(); // this is needed to see the last frame
+    Text gameOverText;
+    gameOverText.setString("GAME OVER");
+    gameOverText.setFont(fnt);
+    sz = GetTextSize(gameOverText);
+    gameOverText.setPosition(400 - (sz.x / 2), 300 - (sz.y / 2));
+    window.draw(gameOverText);
+    window.display();
+    while (true);
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
